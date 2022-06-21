b53800e29f7070053b867d0c71a67b16837a2e94
muhammadjamiu abdulganiyu <105175567+mario0070@users.noreply.github.com>
2022-06-21 08:39:12 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,22 +3,22 @@ var numb1=parseInt(prompt("enter the first number: 0 - 9 "))
 var opr=prompt("enter operator : + , * , - , / , % ")
 var numb2=parseInt(prompt("enter the second number: 0 - 9 "))
 
-if (opr=="+"){
+if (operator=="+"){
     console.log(numb1 + numb2)
 }
 
-else if (opr=="-"){
+else if (operator=="-"){
     console.log(numb1 - numb2)
 }
 
-else if (opr=="/"){
+else if (operator=="/"){
     console.log(numb1 / numb2)
 }
 
-else if (opr=="*"){
+else if (operator=="*"){
     console.log(numb1 * numb2)
 }
 
-else if (opr=="%"){
+else if (operator=="%"){
     console.log(numb1 % numb2)
 }
