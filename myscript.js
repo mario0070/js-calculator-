91c2d6d8b5f47ab70e31a08e2b37c32b134ce99a
muhammadjamiu abdulganiyu <105175567+mario0070@users.noreply.github.com>
2022-06-21 11:47:07 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,33 +3,35 @@ var numb1=parseInt(prompt("enter the first number: 0 - 9 "))
 var opr=prompt("enter operator : + , * , - , / , % ")
 var numb2=parseInt(prompt("enter the second number: 0 - 9 "))
 
-if (opr=="+"){
+if (opr =="+"){
     
-    let result= numb1 + numb2
-    console.log(`${numb1} ${opr} ${numb2} = ${result}`);
+    let result= numb1 + numb2;
+     alert(" result = " + result);
 }
 
-else if (opr=="-"){
+else if (opr =="-"){
 
-    let result= numb1 - numb2
-    console.log(`${numb1} ${opr} ${numb2} = ${result}`);
+    let result= numb1 - numb2;
+     alert(" result = " + result);
 
 }
 
-else if (opr=="/"){
+else if (opr =="/"){
 
-    let result= numb1 / numb2
-    console.log(`${numb1} ${opr} ${numb2} = ${result}`);
+    let result= numb1 / numb2;
+     alert(" result = " + result);
 }
 
-else if (opr=="*"){
+else if (opr =="*"){
 
-    let result= numb1 * numb2
-    console.log(`${numb1} ${opr} ${numb2} = ${result}`);
+    let result= numb1 * numb2;
+     alert(" result = " + result);
+    
 }
 
-else if (opr=="%"){
+else if (opr =="%"){
 
-    let result= numb1 % numb2
-    console.log(`${numb1} ${opr} ${numb2} = ${result}`);
+    let result= numb1 % numb2;
+     alert(" result = " + result);
+  
 }
