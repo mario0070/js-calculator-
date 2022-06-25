860c8b8049bbc30d33de6adbf2de7eb8cfdc477e
muhammadjamiu abdulganiyu <105175567+mario0070@users.noreply.github.com>
2022-06-25 12:04:44 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,38 +3,30 @@ var numb1=parseInt(prompt("enter the first number: 0 - 9 "))
 var opr=prompt("enter operator : + , * , - , / , % ")
 var numb2=parseInt(prompt("enter the second number: 0 - 9 "))
 
-if (opr =="+"){
+if(opr== '+'){
+        result = numb1 + numb2;
+    }
 
-    let result= numb1 + numb2;
-    alert(numb1  + opr + numb2 + " = " + result);
+else if(opr == '-'){
+        result = numb1 - numb2;
+    }
+
+else if(opr == '%'){
+        result = numb1 % numb2;
+    }
+
+else if(opr == '/'){
+        result = numb1 / numb2;
+    }
+
+else if(opr == '*'){
+        result = numb1 * numb2;
+    }
+
+else if(isNaN(numb1) || isNaN(numb2)){
+        
+        alert('incorrect')
+    }
+
+    alert(numb1 + opr + numb2 + '=' + result);
     console.log(numb1 + " " + opr + " " + numb2 + " = " + result);
-}
-
-else if (opr =="-"){
-
-    let result= numb1 - numb2;
-    alert(numb1 + opr + numb2 + " = " + result);
-    console.log(numb1 + " " + opr + " " + numb2 + " = " + result);
-
-}
-
-else if (opr =="/"){
-
-    let result= numb1 / numb2;
-    alert(numb1 + opr + numb2 + " = " + result);
-    console.log(numb1 + " " + opr + " " + numb2 + " = " + result);
-}
-
-else if (opr =="*"){
-
-    let result= numb1 * numb2;
-    alert(numb1 + opr + numb2 + " = " + result);
-    console.log(numb1 + " " + opr + " " + numb2 + " = " + result);
-}
-
-else if (opr =="%"){
-
-    let result= numb1 % numb2;
-    alert(numb1 + opr + numb2 + " = " + result);
-    console.log(numb1 + " " + opr + " " + numb2 + " = " + result);
-}
